bcd6fff7d93bc997ba4303e7db61dcfefe79f8d7
Egor201 <Egorsimikin934@gmail.com>
2025-11-24 03:17:31 +0300

    Update 1.21.10

--- src/main/java/com/HiWord9/RPRenames/mod/gui/widget/RenameButton.java
+++ src/main/java/com/HiWord9/RPRenames/mod/gui/widget/RenameButton.java
@@ -8,10 +8,11 @@ import com.HiWord9.RPRenames.mod.gui.RPRInteractableScreen;
 import com.HiWord9.RPRenames.mod.impl.rename.renderer.builder.AcceptsFavoriteSupplier;
 import com.HiWord9.RPRenames.mod.impl.rename.renderer.builder.AcceptsRPRWidget;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.client.input.Click;
+import net.minecraft.client.gl.RenderPipelines; 
 import net.minecraft.item.Item;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.util.Identifier;
@@ -63,8 +64,9 @@ public class RenameButton extends ClickableWidget implements OffsetableWidget {
     protected void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
         int u = favorite ? FAVORITE_OFFSET_U : 0;
         int v = hovered || (selected && config().highlightSelected) ? FOCUSED_OFFSET_V : 0;
-
+        
         context.drawTexture(
+                RenderPipelines.GUI_TEXTURED,
                 TEXTURE,
                 getX(), getY(),
                 u, v,
@@ -97,7 +99,7 @@ public class RenameButton extends ClickableWidget implements OffsetableWidget {
     }
 
     @Override
-    public boolean mouseClicked(Element.Click click, boolean released) {
+    public boolean mouseClicked(Click click, boolean released) {
         double mouseX = click.x();
         double mouseY = click.y();
         int button = click.button();
