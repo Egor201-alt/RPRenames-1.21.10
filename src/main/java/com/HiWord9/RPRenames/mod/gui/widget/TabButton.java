11ed597e0c94f731ad4037859e9f03c3f98516de
Egor201 <Egorsimikin934@gmail.com>
2025-11-24 19:45:35 +0300

    1.21.10

--- src/main/java/com/HiWord9/RPRenames/mod/gui/widget/TabButton.java
+++ src/main/java/com/HiWord9/RPRenames/mod/gui/widget/TabButton.java
@@ -7,7 +7,7 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 import net.minecraft.client.gui.tooltip.HoveredTooltipPositioner;
 import net.minecraft.client.gui.widget.ClickableWidget;
-import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
@@ -46,7 +46,7 @@ public class TabButton extends ClickableWidget implements OffsetableWidget {
         int v = index * TYPE_OFFSET_V;
 
         context.drawTexture(
-            RenderLayer::getGuiTextured,
+            RenderPipelines.GUI_TEXTURED,
             TEXTURE,
             getX(), getY(),
             (float) u, (float) v,
